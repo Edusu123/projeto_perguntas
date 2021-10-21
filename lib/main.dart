19aed5a3db24c93ce0da47e379b79bf88e70457d
eduardopires <eduardo.c.pires@outlook.com>
2021-10-21 10:19:55 -0300

    Retorno para desenvolvimento do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class PerguntaApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter'),
+        body: Text('Teste commit'),
       ),
     );
   }
