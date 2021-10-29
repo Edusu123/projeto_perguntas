5a0b603236edc436ac052ae5ace63ca73cbf5390
eduardopires <eduardo.c.pires@outlook.com>
2021-10-29 08:56:20 -0300

    Botões para resposta

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/resposta.dart';
 import './questao.dart';
+import './resposta.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -29,18 +31,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
           body: Column(
             children: [
               Questao(perguntas[_perguntaSelecionada]),
-              ElevatedButton(
-                child: Text('Resposta 1'),
-                onPressed: _responder,
-              ),
-              ElevatedButton(
-                child: Text('Resposta 2'),
-                onPressed: _responder,
-              ),
-              ElevatedButton(
-                child: Text('Resposta 3'),
-                onPressed: _responder,
-              ),
+              Resposta('Resposta 1'),
+              Resposta('Resposta 2'),
+              Resposta('Resposta 3'),
             ],
           )),
     );
