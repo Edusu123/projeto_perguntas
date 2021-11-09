3c7161217b7077061f3dd6198293e8311197e875
eduardopires <eduardo.c.pires@outlook.com>
2021-11-09 07:58:10 -0300

    Exibindo Resultado

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
                     ...respostas.map((t) => Resposta(t, _responder)).toList()
                   ],
                 )
-              : null),
+              : Center(
+                  child: Text(
+                    'Parabéns!',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                )),
     );
   }
 }
